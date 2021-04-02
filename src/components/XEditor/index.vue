0c2d4dbabb67cda4121b7df92894fc73333cde3c
D-xuanmo <me@xuanmo.xin>
2021-04-03 00:33:14 +0800

    refactor: 编辑器升级版本

--- src/components/XEditor/index.vue
+++ src/components/XEditor/index.vue
@@ -5,13 +5,7 @@
 <script>
 import {
   createEditor,
-  imageCardComponentExample,
-  todoListComponentExample,
-  jumbotronComponentExample,
-  wordExplainComponentExample,
-  timelineComponentExample,
-  progressComponentExample,
-  stepsComponentExample,
+
   audioTool,
   boldTool,
   cleanTool,
@@ -41,7 +35,20 @@ import {
   insertObjectTool,
   tableTool,
   insertParagraphBeforeTool,
-  insertParagraphAfterTool
+  insertParagraphAfterTool,
+
+  imageCardComponentExample,
+  todoListComponentExample,
+  jumbotronComponentExample,
+  wordExplainComponentExample,
+  timelineComponentExample,
+  progressComponentExample,
+  stepsComponentExample,
+  katexComponentExample,
+
+  TOOLS,
+  COMPONENT_CREATORS,
+  DEVICE_OPTIONS
 } from '@tanbo/textbus'
 import '@tanbo/textbus/bundles/textbus.min.css'
 import uploadFile, { selectFileHandler } from '@/utils/upload'
@@ -68,30 +75,65 @@ export default {
   methods: {
     async _createEditor() {
       editor = createEditor(this.$refs.editor, {
-        componentLibrary: [
-          imageCardComponentExample,
-          todoListComponentExample,
-          jumbotronComponentExample,
-          wordExplainComponentExample,
-          timelineComponentExample,
-          progressComponentExample,
-          stepsComponentExample
-        ],
-        toolbar: [
-          [historyBackTool, historyForwardTool, cleanTool],
-          [insertObjectTool],
-          [headingTool, fontSizeTool, fontFamilyTool],
-          [boldTool, italicTool, strikeThroughTool, underlineTool, colorTool, textBackgroundTool],
-          [lineHeightTool],
-          [insertParagraphBeforeTool, insertParagraphAfterTool],
-          [olTool, ulTool],
-          [textIndentTool],
-          [subscriptTool, superscriptTool],
-          [textAlignTool],
-          [linkTool, unlinkTool],
-          [imageTool, videoTool, audioTool, emojiTool],
-          [tableTool],
-          [findTool]
+        providers: [
+          {
+            provide: TOOLS,
+            useValue: [
+              [historyBackTool, historyForwardTool, cleanTool],
+              [insertObjectTool],
+              [headingTool, fontSizeTool, fontFamilyTool],
+              [boldTool, italicTool, strikeThroughTool, underlineTool, colorTool, textBackgroundTool],
+              [lineHeightTool],
+              [insertParagraphBeforeTool, insertParagraphAfterTool],
+              [olTool, ulTool],
+              [textIndentTool],
+              [subscriptTool, superscriptTool],
+              [textAlignTool],
+              [linkTool, unlinkTool],
+              [imageTool, videoTool, audioTool, emojiTool],
+              [tableTool],
+              [findTool]
+            ]
+          },
+          {
+            provide: DEVICE_OPTIONS,
+            useValue: [{
+              label: 'PC',
+              value: '100%',
+              default: true,
+            }, {
+              label: 'iPhone5/SE',
+              value: '320px'
+            }, {
+              label: 'iPhone6/7/8/X',
+              value: '375px'
+            }, {
+              label: 'iPhone6/7/8 Plus',
+              value: '414px'
+            }, {
+              label: 'iPad',
+              value: '768px'
+            }, {
+              label: 'iPad Pro',
+              value: '1024px'
+            }, {
+              label: 'A4',
+              value: '842px'
+            }]
+          },
+          {
+            provide: COMPONENT_CREATORS,
+            useValue: [
+              imageCardComponentExample,
+              todoListComponentExample,
+              jumbotronComponentExample,
+              wordExplainComponentExample,
+              timelineComponentExample,
+              progressComponentExample,
+              stepsComponentExample,
+              katexComponentExample
+            ]
+          }
         ],
         uploader(type) {
           return new Promise((resolve, reject) => {
@@ -150,6 +192,12 @@ export default {
 
   .textbus-toolbar-action {
     &[title="字体大小"] {
+      margin-right: 5px;
+    }
+  }
+
+  .textbus-status-bar {
+    > div:not(:last-of-type) {
       margin-right: 5px;
     }
   }
